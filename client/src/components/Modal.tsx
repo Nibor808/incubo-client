40020ee681a5726f9bff094aaa39a11fa4b28fce
Robin Erickson <re@incuboweb.ca>
2022-03-26 11:04:37 -0400

    fix modal styles

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-const ModalStyle = {
-    overlay: {
-        position: 'fixed' as 'fixed',
-        zIndex: 9998,
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(129,130,145,0.8)',
-    },
-    content: {
-        maxWidth: '75%',
-        maxHeight: '90%',
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        padding: '15px',
-        borderRadius: '2px',
-        backgroundColor: 'rgb(255,255,255)',
-    },
-};
-
 interface ModalProps {
     isOpen: boolean;
     setIsOpen: (arg: boolean) => void;
@@ -39,18 +14,13 @@ export const Modal: React.FC<ModalProps> = ({isOpen, setIsOpen, headerText, srcT
             appElement={document.getElementById('root') as HTMLElement}
             isOpen={isOpen}
             onRequestClose={() => setIsOpen(false)}
-            style={ModalStyle}
-            contentLabel="Nothing">
-            <h4>{headerText}</h4>
+            overlayClassName="img-modal-overlay"
+            className="img-modal">
+            <div className="img-modal-content">
+                <h4>{headerText}</h4>
 
-            <img
-                src={srcText}
-                alt={headerText}
-                width="100%"
-                style={{
-                    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-                }}
-            />
+                <img src={srcText} alt={headerText} />
+            </div>
         </ReactModal>
     );
 };
